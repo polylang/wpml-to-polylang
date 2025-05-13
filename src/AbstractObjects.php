24618a629be1896c388d3c7450136e5d26b0cdf7
Grégory Viguier <git@screenfeed.fr>
2025-05-13 15:27:50 +0200

    Use `PLL_Translated_Object::set_translation_in_mass()`

--- src/AbstractObjects.php
+++ src/AbstractObjects.php
@@ -7,6 +7,8 @@
 
 namespace WP_Syntex\WPML_To_Polylang;
 
+use PLL_Translated_Object;
+
 defined( 'ABSPATH' ) || exit;
 
 /**
@@ -112,77 +114,20 @@ abstract class AbstractObjects extends AbstractSteppable {
 	 * @return void
 	 */
 	protected function processTranslations( $translations ) {
-		global $wpdb;
+		$translated_object = false;
 
-		$terms = [];
-
-		foreach ( array_keys( $translations ) as $name ) {
-			$terms[] = $wpdb->prepare( '(%s, %s)', $name, $name );
-		}
-
-		$terms = array_unique( $terms );
-
-		// Insert terms.
-		if ( ! empty( $terms ) ) {
-			$wpdb->query( "INSERT INTO $wpdb->terms (slug, name) VALUES " . implode( ',', $terms ) ); // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
-		}
-
-		// Get all terms with their term_id.
-		$terms = $wpdb->get_results(
-			sprintf(
-				"SELECT term_id, slug FROM $wpdb->terms WHERE slug IN ( '%s' )",
-				implode( "', '", esc_sql( array_keys( $translations ) ) )
-			)
-		);
-
-		$tts = [];
-
-		foreach ( $terms as $term ) {
-			$tts[] = $wpdb->prepare(
-				'(%d, %s, %s, %d)',
-				$term->term_id,
-				$this->getTranslationTaxonomy(),
-				serialize( $translations[ $term->slug ] ), // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.serialize_serialize
-				count( $translations[ $term->slug ] )
-			);
-		}
-
-		$tts = array_unique( $tts );
-
-		// Insert term taxonomy part of terms.
-		if ( ! empty( $tts ) ) {
-			$wpdb->query( "INSERT INTO $wpdb->term_taxonomy (term_id, taxonomy, description, count) VALUES " . implode( ',', $tts ) ); // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
-		}
-
-		unset( $terms, $tts ); // Free some memory.
-
-		// Get all terms with their term taxonomy id.
-		$terms = $wpdb->get_results(
-			sprintf(
-				"SELECT tt.term_taxonomy_id, t.slug FROM $wpdb->terms AS t
-				INNER JOIN $wpdb->term_taxonomy AS tt ON t.term_id = tt.term_id
-				WHERE tt.taxonomy = '%s'
-				AND t.slug IN ( '%s' )",
-				esc_sql( $this->getTranslationTaxonomy() ),
-				implode( "', '", esc_sql( array_keys( $translations ) ) )
-			)
-		);
-
-		$trs = [];
-
-		if ( is_array( $terms ) ) {
-			foreach ( $terms as $term ) {
-				foreach ( $translations[ $term->slug ] as $object_id ) {
-					$trs[] = sprintf( '(%d, %d)', (int) $object_id, (int) $term->term_taxonomy_id );
-				}
+		foreach ( PLL()->model->translatable_objects as $translatable_object ) {
+			if ( $translatable_object->get_tax_translations() === $this->getTranslationTaxonomy() ) {
+				$translated_object = $translatable_object;
+				break;
 			}
 		}
 
-		$trs = array_unique( $trs );
-
-		// Insert term relationships.
-		if ( ! empty( $trs ) ) {
-			$wpdb->query( "INSERT INTO $wpdb->term_relationships (object_id, term_taxonomy_id) VALUES " . implode( ',', $trs ) ); // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
+		if ( ! $translated_object instanceof PLL_Translated_Object ) {
+			// Uh?
+			return;
 		}
+
+		$translated_object->set_translation_in_mass( $translations );
 	}
 }
