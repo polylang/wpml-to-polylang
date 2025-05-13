65836435c911c0c8a8c98e1597994a056463c7b8
Grégory Viguier <git@screenfeed.fr>
2025-05-13 15:41:23 +0200

    Customize the name of the terms

--- src/Posts.php
+++ src/Posts.php
@@ -138,7 +138,7 @@ class Posts extends AbstractObjects {
 		// Remove empty ids and group translations by translation group.
 		foreach ( $results as $t ) {
 			if ( ! empty( $t->trid ) && ! empty( $t->language_code ) && ! empty( $t->id ) ) {
-				$translations[ 'pll_wpml_' . $t->trid ][ $t->language_code ] = (int) $t->id;
+				$translations[ 'wpml_' . $t->trid ][ $t->language_code ] = (int) $t->id;
 			}
 		}
 
